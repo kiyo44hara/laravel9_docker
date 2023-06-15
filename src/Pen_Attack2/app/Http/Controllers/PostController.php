07d6e4fbe4b1d77d1a33cb2297731c820a19f101
EmikaKiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-06-15 20:21:29 +0900

    [投稿機能バリデーション設定中]

--- src/Pen_Attack2/app/Http/Controllers/PostController.php
+++ src/Pen_Attack2/app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 
 class PostController extends Controller
@@ -16,7 +17,23 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        // 投稿の保存ロジックを実装してください
+        $rules = [
+            'title' => ['required', 'string', 'max:50'],
+            'content' => ['required', 'string', 'max:1000'],
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return Inertia::location('/posts');
     }
 
     public function show($id)
@@ -27,11 +44,11 @@ class PostController extends Controller
 
     public function update(Request $request, $id)
     {
-        // 投稿の更新ロジックを実装してください
+        // 投稿の更新
     }
 
     public function destroy($id)
     {
-        // 投稿の削除ロジックを実装してください
+        // 投稿の削除
     }
 }
--- src/Pen_Attack2/resources/js/Pages/Admin/Post/Index.jsx
+++ src/Pen_Attack2/resources/js/Pages/Admin/Post/Index.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { Box, Heading, FormControl, FormLabel, Input, Textarea, Button } from '@chakra-ui/react';
 import { Inertia } from '@inertiajs/inertia';
 
-const index = () => {
+import { ChakraProvider } from '@chakra-ui/react'; // 追加
+
+const Index = () => { // コンポーネント名を Index に修正
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
@@ -26,38 +28,40 @@ const index = () => {
   };
 
   return (
-    <Box maxW="md" mx="auto" mt={8} p={4}>
-      <Heading as="h1" size="lg" mb={4}>
-        新規投稿
-      </Heading>
+    <ChakraProvider> {/* 追加 */}
+      <Box maxW="md" mx="auto" mt={8} p={4}>
+        <Heading as="h1" size="lg" mb={4}>
+          新規投稿
+        </Heading>
 
-      <form onSubmit={handleSubmit}>
-        <FormControl mb={4}>
-          <FormLabel>タイトル</FormLabel>
-          <Input
-            type="text"
-            value={title}
-            onChange={handleTitleChange}
-            placeholder="タイトルを入力してください"
-          />
-        </FormControl>
+        <form onSubmit={handleSubmit}>
+          <FormControl mb={4}>
+            <FormLabel>タイトル</FormLabel>
+            <Input
+              type="text"
+              value={title}
+              onChange={handleTitleChange}
+              placeholder="タイトルを入力してください"
+            />
+          </FormControl>
 
-        <FormControl mb={4}>
-          <FormLabel>内容</FormLabel>
-          <Textarea
-            value={content}
-            onChange={handleContentChange}
-            placeholder="内容を入力してください"
-            rows={6}
-          />
-        </FormControl>
+          <FormControl mb={4}>
+            <FormLabel>内容</FormLabel>
+            <Textarea
+              value={content}
+              onChange={handleContentChange}
+              placeholder="内容を入力してください"
+              rows={6}
+            />
+          </FormControl>
 
-        <Button type="submit" colorScheme="blue">
-          投稿する
-        </Button>
-      </form>
-    </Box>
+          <Button type="submit" colorScheme="blue">
+            投稿する
+          </Button>
+        </form>
+      </Box>
+    </ChakraProvider> 
   );
 };
 
-export default index;
+export default Index;
